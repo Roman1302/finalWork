54e4af776e683c7be87a1dabc53a1c0ac472d947
Роман Бухмистов <rrb13@mail.ru>
2022-09-20 16:48:52 +0300

    Написана программа отбора значений

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Написать прогрмамму, которая из имеющегося массива строк формирует массив из строк, 
+длина которых меньше либо равна 3 симвала. */
+
+string SelectingValues(string[] array)
+{
+    int numberСharacters = 0;
+    int length = array.Length;
+    int i = 0;
+    int j = 0;
+    string[] arrNew = new string[length];
+    while (i < length)
+    {
+        string text = array[i];
+        numberСharacters = text.Length;
+        if (numberСharacters >= 3)
+        {
+            arrNew[j] = array[i];
+            Console.Write($"{arrNew[j]} ");
+            j++;
+        }
+        i++;
+    }
+    return arrNew[j];
+}
+
+Console.Clear();
+Console.WriteLine("***Программа вывода массив из строк, длина которых меньше либо равна 3 симвала***");
+Console.WriteLine();
+Console.WriteLine("Ведите массив любых значений через пробел:");
+string[] arrayValues = Console.ReadLine()!.Split();
+Console.Write(" -> ");
+SelectingValues(arrayValues);
+Console.WriteLine();
